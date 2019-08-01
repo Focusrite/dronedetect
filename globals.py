40c4363c4f2a0b08ed03e7bbc5db401e9b5abc56
Usern User <user@no.no>
2019-08-01 13:01:31 +0200

    Added globals.py

---
+++ globals.py
@@ -0,0 +1,9 @@
+def initialize():
+    global started
+    global latitude
+    global longitude
+    global altitude
+    latitude = 58.4035
+    longitude = 15.6850
+    altitude = 55
+    started = False

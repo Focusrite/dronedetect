7fc3529d24fd0bc5ed00388bb860c224f920240e
Usern User <user@no.no>
2019-08-07 11:00:19 +0200

    Changed name to calculate_r_and_t.py

--- calculate_r_and_t.py
+++ calculate_r_and_t.py
@@ -1,41 +1,9 @@
+# calculate_r_and_t.py
+
 import cv2 as cv
 import numpy as np
 import math
 from capture import Capture
-
-# Checks if a matrix is a valid rotation matrix.
-def isRotationMatrix(R) :
-    Rt = np.transpose(R)
-    shouldBeIdentity = np.dot(Rt, R)
-    I = np.identity(3, dtype = R.dtype)
-    n = np.linalg.norm(I - shouldBeIdentity)
-    return n < 1e-6
- 
- 
-# Calculates rotation matrix to euler angles
-# The result is the same as MATLAB except the order
-# of the euler angles ( x and z are swapped ).
-def rotationMatrixToEulerAngles(R) :
- 
-    assert(isRotationMatrix(R))
-     
-    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
-     
-    singular = sy < 1e-6
- 
-    if  not singular :
-        x = math.atan2(R[2,1] , R[2,2])
-        y = math.atan2(-R[2,0], sy)
-        z = math.atan2(R[1,0], R[0,0])
-    else :
-        x = math.atan2(-R[1,2], R[1,1])
-        y = math.atan2(-R[2,0], sy)
-        z = 0
-
-    x = x * 180 / math.pi
-    y = y * 180 / math.pi
-    z = z * 180 / math.pi
-    return np.array([x, y, z])
 
 # Function that calculates R_1to2 and t_1to2 and saves them to file_name
 # The camera matrices and distortion coefficients have to be calculated
@@ -61,8 +29,8 @@ def find_r_and_t(img1, img2, file_name="r_and_t.xml", cam1="camera1.xml", cam2="
     fs2.release()
 
     # Calculate the rvecs and tvecs
-    ret1, rvec1, tvec1, inliers = cv.solvePnPRansac(objp, corners1, c_matrix1, dist1, reprojectionError=2.0)#, flags = cv.SOLVEPNP_ITERATIVE)
-    ret2, rvec2, tvec2, inliers = cv.solvePnPRansac(objp, corners2, c_matrix2, dist2, reprojectionError=2.0)#, flags = cv.SOLVEPNP_)
+    ret1, rvec1, tvec1, inliers = cv.solvePnPRansac(objp, corners1, c_matrix1, dist1, reprojectionError=2.0)
+    ret2, rvec2, tvec2, inliers = cv.solvePnPRansac(objp, corners2, c_matrix2, dist2, reprojectionError=2.0)
 
     # Calculate rotation matrices from rvecs
     R1, _ = cv.Rodrigues(rvec1)
@@ -74,8 +42,6 @@ def find_r_and_t(img1, img2, file_name="r_and_t.xml", cam1="camera1.xml", cam2="
     print("tvec2\n", tvec2)
 
     # Calculate rotation matrix and translation vector from camera 1 to 2
-    #R_1to2 = np.matmul(R2, np.transpose(R1))
-    #t_1to2 = np.matmul(R2, (-1 * np.matmul(np.transpose(R1), tvec1))) + tvec2
     R_1to2 = R2@R1.T
     t_1to2 = tvec2 -R_1to2@tvec1
 
@@ -86,33 +52,26 @@ def find_r_and_t(img1, img2, file_name="r_and_t.xml", cam1="camera1.xml", cam2="
     print("write t\n", t_1to2)
     fs.write("t", t_1to2)
     fs.release()
-
-    print(rotationMatrixToEulerAngles(R_1to2))
     
 if __name__ == '__main__':
-    timer = cv.getTickCount()
-    path1 = "chessboard_cam1.png"
-    path2 = "chessboard_cam2.png"
 
     # Serial numbers for the two cameras
     serial1 = "40016577"
     serial2 = "21810700"
 
+    # Name of output file
+    output_file = "r_and_t.xml"
+
     # Create Capture objects for the cameras
     cap1 = Capture(serial1)
     cap2 = Capture(serial2)
-
     cap1.start()
     cap2.start()
 
-    path1 = cap1.grab()
-    path2 = cap2.grab()
+    img1 = cap1.grab()
+    img2 = cap2.grab()
 
-    find_r_and_t(path1, path2)
+    find_r_and_t(img1, img2)
 
     cap1.stop()
     cap2.stop()
-
-    time = cv.getTickCount() - timer
-    time = time / cv.getTickFrequency()
-    print(time)
